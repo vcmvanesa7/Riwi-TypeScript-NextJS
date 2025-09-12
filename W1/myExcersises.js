4e63c48e61ec8c609fd7183fe841aeb0e2b6b615
vcmvanesa7 <vcmvanesamedina7@gmail.com>
2025-09-12 08:27:29 -0500

    cambio minimo

--- W1/myExcersises.js
+++ W1/myExcersises.js
@@ -4,4 +4,22 @@ function printCoord(coordenada) {
 }
 ;
 printCoord({ x: 123, y: 456, });
-//
+;
+;
+var Empleado2 = {
+    nombre: "Juan",
+    edad: 25,
+    salario2: 2000
+};
+var Empleado = {
+    nombre: "Juan",
+    edad: 25,
+    salario: 2000
+};
+var desition = "Yes";
+var User = {
+    name: "Vanesa",
+    lastName: "Carrillo",
+    edad: 29
+};
+var sum = function (a, b, c) { return a + b + c; };
