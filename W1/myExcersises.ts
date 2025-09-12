dabcef9fa67cda442d65c3eec2a330a4c0c7563e
Vanesa Carrillo <vcmvanesamedina7@gmail.com>
2025-09-11 19:36:13 -0500

    Update myExcersises.ts
    Add more examples about week1 typescript official documentation.

--- W1/myExcersises.ts
+++ W1/myExcersises.ts
@@ -39,4 +39,62 @@ function printCoord (coordenada : point) {
 
 printCoord({x:123,y:456,})
 
-//
+
+// --- DIFERENCES BETWEEN TYPE AND INTERFACE ---
+
+// 1. Both can extend but in diffrent ways
+// ---interface
+interface persona2 {
+  nombre: string;
+  edad: number;
+};
+interface empleado2 extends persona2 { // Extends
+  salario2 : number
+};
+const Empleado2 : empleado2 = {
+  nombre : "Juan",
+  edad : 25,
+  salario2 : 2000
+}
+
+
+// ---Type
+type persona = {
+  nombre: string;
+  edad: number;
+};
+type empleado = persona & {   // Extends
+  salario : number
+}
+const Empleado : empleado = {
+  nombre : "Juan",
+  edad : 25,
+  salario : 2000
+}
+
+//2. Union of types (Only Type)
+//This can't be done with 'interface'
+
+type Answer = "Yes" | "No" |"Maybe"
+let desition : Answer = "Yes";
+
+//3. Reopen or merge statements (Interface Only)
+interface user {
+  name : string;
+}
+ interface user {
+  lastName : string,
+  edad : number
+ }
+
+ const User : user = {
+   name: "Vanesa",
+   lastName: "Carrillo",
+   edad: 29
+ }
+
+//4. Use primitives types (Type Only)
+//Interface cannot define primitive types or functions directly
+
+type addition = (a:number, b:number, c: number) => number;
+const sum : addition = (a,b,c ) => a + b + c;
